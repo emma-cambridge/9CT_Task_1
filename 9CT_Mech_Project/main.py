ebdcfc72567960e2f859828c2e71f234c9d8297f
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-04-08 11:06:44 +1000

    Object Detection Function
    Coded object detection and started on the colour check function with difficulty - I can't get the colour sensor to detect colours yet

--- 9CT_Mech_Project/main.py
+++ 9CT_Mech_Project/main.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.ev3devices import Motor, UltrasonicSensor
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
 
+obstacle_sensor = UltrasonicSensor(Port.S4)
+colour_sensor = ColorSensor(Port.S3)
 
-# Write your program here.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
 ev3.speaker.beep()
 
-# This is object_detection. I'll work out where to put it later
-while distancce >= 100:
-    Drive(100)
+while True:
+    robot.drive(200, 0)
+
+    while obstacle_sensor.distance() > 100:
+        robot.drive(100, 0)
+
+    if colour_sensor.color() == Color.GREEN:
+        robot.stop
+    else:
+        ev3.speaker.beep()
