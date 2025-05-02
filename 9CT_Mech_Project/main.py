bd3c8cdc3260d19432c2cfd5af072b3f806c09d2
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-05-02 12:28:25 +1000

    Trial and Error
    Experimented with the colour sensor and got it working but decided that it might be better to code the robot to make it not fall off the mat.

--- 9CT_Mech_Project/main.py
+++ 9CT_Mech_Project/main.py
@@ -3,9 +3,10 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Color
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
+
 
 ev3 = EV3Brick()
 
@@ -17,15 +18,18 @@ right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-ev3.speaker.beep()
+robot.drive(100, 0)
 
 while True:
-    robot.drive(200, 0)
 
     while obstacle_sensor.distance() > 100:
         robot.drive(100, 0)
 
+    
+    
     if colour_sensor.color() == Color.GREEN:
-        robot.stop
+        stop()
+    elif colour_sensor.color() == Color.BLUE:
+        stop()
     else:
         ev3.speaker.beep()
