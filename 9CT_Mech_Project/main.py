5a436309cdef67f28b7612e6d044eeee6e936883
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-05-06 11:08:54 +1000

    Made Robot Path
    Did good made angles go wow wow yeah

--- 9CT_Mech_Project/main.py
+++ 9CT_Mech_Project/main.py
@@ -12,7 +12,7 @@ right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-robot.straight(200)
+robot.straight(180)
 ev3.speaker.beep()
 
 robot.turn(-110)
@@ -20,3 +20,24 @@ ev3.speaker.beep()
 
 robot.straight(300)
 ev3.speaker.beep()
+
+#grabby grabby process here
+
+robot.straight(230) # small amount
+
+robot.turn(-45) # eensie weensie bit
+robot.turn(-45)
+
+#grabby grabby
+
+robot.straight(395)
+
+robot.turn(-45)
+robot.turn(-45)
+robot.turn(-20)
+
+robot.straight(550)
+
+robot.turn(-110)
+
+robot.straight(100)
