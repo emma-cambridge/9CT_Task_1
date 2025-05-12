1e176110a2b27905903a673a4643707568e015e7
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-05-12 12:28:36 +1000

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
    NOTHING IS WORKING EVERYTHING IS BAD BAD BAD AHHHHHHHHHHHH

--- 9CT_Mech_Project/main.py
+++ 9CT_Mech_Project/main.py
@@ -1,43 +1,56 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.parameters import Port
+from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
 ev3 = EV3Brick()
+
+ultrasonic_sensor = UltrasonicSensor(Port.S1)
 
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
+robot.turn(-10)
+robot.turn(-10)
 robot.straight(180)
-ev3.speaker.beep()
+robot.turn(-40)
+robot.turn(-42)
 
-robot.turn(-110)
-ev3.speaker.beep()
+robot.straight(500)
 
-robot.straight(300)
-ev3.speaker.beep()
+#if ultrasonic_sensor.distance() > 200:
+    #ev3.speaker.beep
 
-#grabby grabby process here
 
-robot.straight(230) # small amount
 
-robot.turn(-45) # eensie weensie bit
-robot.turn(-45)
 
-#grabby grabby
+#ev3.speaker.beep()
 
-robot.straight(395)
+#robot.turn(-110)
+#ev3.speaker.beep()
 
-robot.turn(-45)
-robot.turn(-45)
-robot.turn(-20)
+#robot.straight(300)
+#ev3.speaker.beep()
 
-robot.straight(550)
 
-robot.turn(-110)
+#robot.straight(230)
 
-robot.straight(100)
+#robot.turn(-50)
+#robot.turn(-50)
+
+#robot.straight(395)
+
+#robot.turn(-45)
+#robot.turn(-45)
+#robot.turn(-20)
+
+#robot.straight(550)
+
+#robot.turn(-110)
+
+#robot.straight(90)
