364ffb63808f594ef298f581c93806c5f666ecf6
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-05-13 12:26:46 +1000

    Almost Final
    Got the touch sensor working first try! Also made some other little changes

--- 9CT_Mech_Project/main.py
+++ 9CT_Mech_Project/main.py
@@ -2,6 +2,7 @@
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor
+from pybricks.ev3devices import TouchSensor, Motor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
@@ -15,42 +16,33 @@ right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-robot.turn(-10)
-robot.turn(-10)
-robot.straight(180)
-robot.turn(-40)
-robot.turn(-42)
+touch_sensor = TouchSensor(Port.S2)
 
-robot.straight(500)
+while True:
+    if touch_sensor.pressed():
+        ev3.speaker.beep()
 
-#if ultrasonic_sensor.distance() > 200:
-    #ev3.speaker.beep
+        robot.turn(-10)
+        robot.turn(-10)
+        robot.straight(180)
+        robot.turn(-40)
+        robot.turn(-42)
 
+        robot.straight(500)
 
+        if ultrasonic_sensor.distance() > 200:
+            ev3.speaker.beep
 
+        robot.turn(-50)
+        robot.turn(-50)
 
-#ev3.speaker.beep()
+        robot.straight(450)
 
-#robot.turn(-110)
-#ev3.speaker.beep()
+        robot.turn(-45)
+        robot.turn(-45)
+        robot.turn(-20)
 
-#robot.straight(300)
-#ev3.speaker.beep()
+        robot.straight(520)
 
-
-#robot.straight(230)
-
-#robot.turn(-50)
-#robot.turn(-50)
-
-#robot.straight(395)
-
-#robot.turn(-45)
-#robot.turn(-45)
-#robot.turn(-20)
-
-#robot.straight(550)
-
-#robot.turn(-110)
-
-#robot.straight(90)
+        robot.turn(-110)
+        robot.straight(130)
