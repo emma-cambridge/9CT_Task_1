f61e22ad1c8d94b3fe47c37448072587e7f1e7bf
emma-cambridge <emma.cambridge1@education.nsw.gov.au>
2025-05-17 20:21:42 +1000

    Finished Up Theory Work
    Documented test cases, finished evaluation, and added comments to the code. Very happy but might review work tomorrow before being done done

--- 9CT_Mech_Project/main.py
+++ 9CT_Mech_Project/main.py
@@ -9,40 +9,47 @@ from pybricks.robotics import DriveBase
 
 ev3 = EV3Brick()
 
+# Initialises the sensors
 ultrasonic_sensor = UltrasonicSensor(Port.S1)
+touch_sensor = TouchSensor(Port.S2)
 
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-touch_sensor = TouchSensor(Port.S2)
-
 while True:
+    # Touch sensor starts the program if it's pressed
     if touch_sensor.pressed():
         ev3.speaker.beep()
-
+    
+        # This is the robot's first turn on the bottom right corner
         robot.turn(-10)
         robot.turn(-10)
         robot.straight(180)
         robot.turn(-40)
         robot.turn(-42)
 
+        # This is the robot going from the bottom right corner to the yellow block
         robot.straight(500)
 
+        # If an object is 20cm away from the robot it will beep
         if ultrasonic_sensor.distance() > 200:
             ev3.speaker.beep
 
+        # Turn to retrieve the red block
         robot.turn(-50)
         robot.turn(-50)
 
+        # Top right to left, collects red block on the way
         robot.straight(440)
 
+        # From top left to bottom left
         robot.turn(-45)
         robot.turn(-45)
         robot.turn(-20)
-
         robot.straight(520)
 
+        # Turns and pushes blocks into start zone
         robot.turn(-100)
         robot.straight(40)
